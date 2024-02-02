de6e6a09689bdbf66e6f3ab7478f4b6f74f7ec3b
charendra910 <charendra910@rku.ac.in>
2024-02-02 18:59:39 +0530

    add student done

---
+++ Controllers/AddStudentController1.cs
@@ -0,0 +1,24 @@
+﻿using aspcoredemo.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspcoredemo.Controllers
+{
+    public class AddStudentController1 : Controller
+    {
+        private studentdbcontext db;
+
+        public AddStudentController1(studentdbcontext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
--- Migrations/20240119033054_add student.Designer.cs
+++ Migrations/20240119033054_add student.Designer.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using aspcoredemo.Data;
 
-#nullable disabled
+//nullable disabled
 
 namespace aspcoredemo.Migrations
 {
